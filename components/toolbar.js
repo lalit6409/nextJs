9c533f3881276487a91f7e1ae439dd40ee282537
Lalit Kumar mehta <lalitkumarmehta@Lalits-MacBook-Air.local>
2021-11-10 10:41:14 +0530

    next js portfolio, commit 4

--- components/toolbar.js
+++ components/toolbar.js
@@ -9,13 +9,17 @@ export const Toolbar = () => {
       <div onClick={() => router.push("/")}>Home</div>
       <div onClick={() => router.push("/about")}>About Me</div>
       <div>
-        <a href="https://twitter.com/mehtalalit6409" target="_blank">
+        <a
+          href="https://twitter.com/mehtalalit6409"
+          target="_blank"
+          rel="noreferrer"
+        >
           Twitter
         </a>
       </div>
 
       <div>
-        <a href="https://github.com/lalit6409" target="_blank">
+        <a href="https://github.com/lalit6409" target="_blank" rel="noreferrer">
           GitHub
         </a>
       </div>
